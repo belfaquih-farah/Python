9d4e7a958826e7eca036815c2216707154fb0f14
Belfaquih Farah <farah.belfaquih@etu.uae.ac.ma>
2026-03-16 21:30:04 +0000

    TP Django

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views  
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+]
+
+#def logout(request):
+ #   auth.logout(request)
+  #  return redirect('index')
